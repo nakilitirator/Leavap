13e3e851b9fb576f73f7c90c2a0776b0434d0dc4
Leavap <pahagolik@mail.ru>
2024-08-10 13:51:50 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,19 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_homework_assignments
+
+print('Курс:', course_name, f'всего задач:{number_of_completed_homework_assignments}, затрачено часов: {number_of_hours_spent}, среднее время выполнения', time_for_one_task, 'часа.')
+
+
+
+
+
+
+#Напишите 4 переменных которые буду обозначать следующие данные:
+#Количество выполненных ДЗ (запишите значение 12)
+#Количество затраченных часов (запишите значение 1.5)
+#Название курса (запишите значение 'Python')
+#Время на одно задание (вычислить используя 1 и 2 переменные)
+#Выведите на экран(в консоль), используя переменные, следующую строку:
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
