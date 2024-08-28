0e4416fb42ce3fbb3162d14da17e4d18472080ac
Leavap <pahagolik@mail.ru>
2024-08-28 14:41:51 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,12 @@
-def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    if ('@' and ('.com' or '.ru' or '.net')) not in (recipient and sender):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif sender == recipient:
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    if sender == recipient or recipient == sender:
         print('Нельзя отправить письмо самому себе!')
+    elif ('@' and ('.com' or '.ru' or '.net')) not in (recipient and sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif sender == 'university.help@gmail.com':
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-    elif sender != 'university.help@gmail.com':
+    else:
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
