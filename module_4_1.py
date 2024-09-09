257a8dccffd47b8080c75cc67ed2a13df87f621d
Leavap <pahagolik@mail.ru>
2024-09-09 20:33:08 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+from true_math import divide as tm
+from fake_math import divide as fm
+
+true_divide = tm
+fake_divide = fm
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
