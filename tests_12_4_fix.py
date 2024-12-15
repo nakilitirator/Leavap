f8fcdeee06a0a2278ecb01d7c549cde46509f77c
Leavap <pahagolik@mail.ru>
2024-12-15 10:05:54 +0700

    Update tests_12_4_fix.py

--- tests_12_4_fix.py
+++ tests_12_4_fix.py
@@ -9,8 +9,8 @@ class RunnerTest(unittest.TestCase):
             hodba = Runner('Ходьба', -5)
             for i in range(10):
                 hodba.walk()
-            self.assertEqual(hodba.distance, 50)
             logging.info('"test_walk" выполнен успешно')
+            self.assertEqual(hodba.distance, 50)
         except ValueError:
             logging.warning('Неверная скорость для Runner', exc_info=True)
 
@@ -19,15 +19,14 @@ class RunnerTest(unittest.TestCase):
             beg = Runner(25896)
             for i in range(10):
                 beg.run()
-            self.assertEqual(beg.distance, 100)
             logging.info('"test_run" выполнен успешно')
+            self.assertEqual(beg.distance, 100)
         except TypeError:
             logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
 
 
 if __name__ == '__main__':
-    unittest.main()
     logging.basicConfig(level=logging.INFO, filemode='w',
                         filename='runner_tests.log', encoding='utf-8',
                         format='%(asctime)s | %(levelname)s | %(message)s')
-
+    unittest.main()
